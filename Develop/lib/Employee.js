1149c852f3407944adc071eb815d6159623c2987
Jake Wagner <jkwagneriii@gmail.com>
2020-10-19 20:39:31 -0500

    crushed through the employee constructor class. syntax seems to match reference code on w3 and logs are working. Now onto engineer?

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -5,7 +5,24 @@ class Employee {
         this.name = name;
         this.email = email;
     }
+
+    //The following mehtods are nn reference to the htmlRenderer.js file. Each method will contain a return using the 'this' keyword.
+    getId() {
+        return this.id
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return Employee
+    }   
 }
 
-
+//Export employee data down the chain of specific roles within the team.
 module.exports = Employee;

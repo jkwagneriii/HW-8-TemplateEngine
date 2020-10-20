4ca630cbf9369b3782d8453d20309a3f161debf9
Jake Wagner <jkwagneriii@gmail.com>
2020-10-19 20:30:40 -0500

    started working on the contructor for the employee.js and a litte on the engineer.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,11 @@
-// TODO: Write code to define and export the Employee class
+//This script will use a class called Employee to create a constructor that takes in user data. Using a class creates the oppourtunity to add multiple employees without repeating code.
+class Employee {
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
+
+
+module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,8 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./lib/Employee");
+
+const Engineer = function(gitUserName) {
+    this.gitUserName = gitUserName;
+}
+
+module.exports = Engineer;

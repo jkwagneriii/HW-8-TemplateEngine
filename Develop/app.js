f0262d4fdd8059245ba0c04c44b8b322698fb50f
Jake Wagner <jkwagneriii@gmail.com>
2020-10-19 21:11:21 -0500

    created question array(s) confirmed on the console.

--- Develop/app.js
+++ Develop/app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+//Umm, what is this supposed to do Jake?
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -13,6 +14,73 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+//Empty array to store individual objects
+let productionTeam = [];
+
+const mainQuestions = [
+    {
+        name: 'id',
+        type: 'input',
+        message: 'What is your employee ID?'
+    },
+    {
+        name: 'name',
+        type: 'input',
+        message: 'What is your name?'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'What is your email address?'
+    },
+    {
+        name: 'role',
+        type: 'list',
+        message: 'Select your current company role.',
+        choices: ["Manager", "Engineer", "Intern"]
+    }
+]
+
+//If the user selects manager they will be asked...
+const managerQuestions = [
+    {
+        name: 'officeNumber',
+        type: 'input',
+        message: 'What is your office phone number?'
+    }
+]
+
+//If the user selects engineer they will be asked...
+const engineerQuestions = [
+    {
+        name: 'github',
+        type: 'input',
+        message: 'What is your Github username?'
+    }
+]
+
+//If the user selects intern they will be asked...
+const internQuestions = [
+    {
+        name: 'school',
+        type: 'input',
+        message: 'What school do you attend?'
+    }
+]
+
+console.log(mainQuestions);
+
+
+
+
+
+
+
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
